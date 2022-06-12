b3fe18d417676e31bf43078b26cbe499f8efa655
Nanda Wawan K <nanda@terralogiq.com>
2022-06-12 20:33:57 +0700

    challenge-07

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (category) => {
+  let count = 0;
+  const theaterVGC = await promiseTheaterVGC();
+  const theaterIXX = await promiseTheaterIXX();
+
+  count += theaterVGC.filter(row => row.hasil === category).length;
+  count += theaterIXX.filter(row => row.hasil === category).length;
+
+  return count;
+};
 
 module.exports = {
   promiseOutput,
